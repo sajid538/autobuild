1ba2aca052f3df3181d1e4a62a090a5c50f3e00b
ybmadhu <ybmadhu707@gmail.com>
2020-02-19 17:39:32 +0530

    Update App.java

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World .. madhu JMS technologies." );
+        System.out.println( "Hello World .. madhu ." );
     }
 
 	public int calculateSomething() {
